72094abf28a8853bdbf180b767797142d2da14c5
idrisimo <idrissilva@hotmail.com>
2022-05-19 13:06:53 +0100

    still trying to deploy

--- api/projects/views.py
+++ api/projects/views.py
@@ -11,4 +11,5 @@ from .serializers import ProjectsSerializer
 def get_projectsAPI(request):
     projects = Projects.objects.all()
     serializer = ProjectsSerializer(projects, many=True)
+    
     return Response(serializer.data)
