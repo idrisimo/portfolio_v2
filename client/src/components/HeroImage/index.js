4509d4ce1dc75bba696a6bc814bd503c43e37797
idrisimo <idrissilva@hotmail.com>
2022-06-07 16:35:29 +0100

    change to CV link

--- client/src/components/HeroImage/index.js
+++ client/src/components/HeroImage/index.js
@@ -6,15 +6,12 @@ import Button from 'react-bootstrap/Button'
 export const HeroImage = () => {
     
     return (
-        // <div style={{backgroundImage: `linear-gradient(to bottom, #212529, rgba(255,255,255,0.5)),url(${bgImage})`}}  className='jumbotron'>
-        //     <div className='container text-light text-start'>
-        // <div style={{backgroundImage: `linear-gradient(to bottom, #212529, rgba(255,255,255,0.0)),url(${bgImage})`}} className='jumbotron'>
         <div>
             <div className='container text-light text-start'>
                 <h1 className='display-3 pt-5'>Idris Silva</h1>
                 <p className='lead'>Software Developer</p>
                 <hr></hr>
-                <Button className='mx-2 shadow' href='https://idrissilva-portfolio-v2.s3.eu-west-2.amazonaws.com/Idris_Silva_fp_cv.pdf' target="_blank" rel="noopener noreferrer" >Resume</Button>
+                <Button className='mx-2 shadow' href='https://idrissilva-portfolio-v2.s3.eu-west-2.amazonaws.com/Idris+Silva+IT+CV+v4.pdf' target="_blank" rel="noopener noreferrer" >Resume</Button>
             </div>
         </div>
     )
