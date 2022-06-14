52bafb1559eae24ad66a419198693c92cd2b418e
idrisimo <idrissilva@hotmail.com>
2022-06-14 17:07:16 +0100

    added new db config to connect postgress heroku

--- api/api/settings.py
+++ api/api/settings.py
@@ -93,7 +93,12 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-SQL_ALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
+# SQL_ALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
